5ddd3391a7055b2bfb1c2d9914a84a54894dcbaa
zoeller tom <zoellertom@dv-schulen.de>
2019-12-18 14:14:46 +0100

    Initial commit

---
+++ app/src/main/java/com/example/projektarbeit/Registrierung.java
@@ -0,0 +1,14 @@
+package com.example.projektarbeit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Registrierung extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrierung);
+    }
+}
